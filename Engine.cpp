edc309a9ff424afb1a6b0d0fc8982a762fe55504
Sebastian Hruszczak <sebastian.hruszczak@gmail.com>
2022-02-04 13:12:05 +0100

    Transparent grid representation; XO texture;

--- Engine.cpp
+++ Engine.cpp
@@ -10,7 +10,7 @@ void Game::initialize_Variable()
 void Game::initialize_Window()
 {
 	this->video_mode.width = 600;
-	this->video_mode.height = 600;
+	this->video_mode.height = 800;
 	this->window_ptr = new sf::RenderWindow(this->video_mode, "SFML window");
 	this->window_ptr->setFramerateLimit(60);
 }
@@ -21,15 +21,50 @@ void Game::initialize_Music()
 	if (!this->music.openFromFile("sound.ogg"))
 		exit(1);
 }
-//Grid texture initialization 
+
+//texture initialization 
 void Game::initialize_Grid()
 {
 	// Load textures
 	if (!this->texture_background.loadFromFile("1.png"))
 		exit(1);
 
+	if (!this->texture_o.loadFromFile("2.png"))
+		exit(1);
+
+	if (!this->texture_x.loadFromFile("3.png"))
+		exit(1);
+
+
+
+	float x = 0.f, y = 0.f;
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)		//Set every square position,size,color and outline
+		{
+			this->grid[i][j].setPosition(x, y);
+			this->grid[i][j].setSize(sf::Vector2f(200.f, 200.f));
+			this->grid[i][j].setFillColor(sf::Color(162, 162, 162, 0));
+			//this->grid[i][j].setOutlineThickness(1.f);					Debug only (position check)
+			//this->grid[i][j].setOutlineColor(sf::Color(0, 0, 0, 255));
+			y += 200;
+		}
+		y = 0.f;		//set valuses for nex loop
+		x += 200.f;
+	}
+
+
 	// Create a sprite of grid
 	this->background.setTexture(texture_background);
+
+
+	///  create x/o vector?
+	this->shape_o.setTexture(texture_o);
+	this->shape_x.setTexture(texture_x);
+
+	this->shape_o.setPosition(20.f, 410.f);
+	this->shape_x.setPosition(20.f, 210.f);
 
 }
 
@@ -42,13 +77,13 @@ Game::Game()
 	this->initialize_Music();
 	this->music.play();
 	this->initialize_Grid();
-
 }
 
 Game::~Game()
 {
 	delete this->window_ptr;
 }
+
 
 ////////////////////	Functions	////////////////////
 
@@ -71,14 +106,14 @@ void Game::events_Pool()
 	{
 		switch (this->event_1.type)
 		{
-		case sf::Event::Closed:			//Pressing x on window
+		case sf::Event::Closed:				//Pressing x on window
 			this->window_ptr->close();
 			break;
 		}
 	}
 }
 
-//Update 
+//Update			//game logic
 void Game::update()
 {
 	this->events_Pool();	//handle events
@@ -93,10 +128,21 @@ void Game::update()
 //Clearing old frame -> drawing objects -> displaying new frame
 void Game::render()
 {
-	this->window_ptr->clear();		
+	this->window_ptr->clear();
 
 	//Draw here
 	this->window_ptr->draw(background);
+	this->window_ptr->draw(shape_x);
+	this->window_ptr->draw(shape_o);
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			this->window_ptr->draw(grid[i][j]);
+		}
+	}
 
 
 	this->window_ptr->display();
--- Engine.h
+++ Engine.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <iostream>
 
 //Game engine
 
@@ -21,8 +22,20 @@ private:
 	sf::Texture texture_background;
 	sf::Sprite background;
 
+	//Shapes
+	sf::Texture texture_o;
+	sf::Texture texture_x;
+
+	std::vector<sf::Sprite> shape_b;		//fun tworz¹ca x y pobieraj¹ca pozycje myszy i ³¹cz¹ca j¹ z przestrzeni¹
+	std::vector<sf::Sprite> shape_c;
+	sf::RectangleShape grid[3][3];
+
+	sf::Sprite shape_x;
+	sf::Sprite shape_o;
+
 	//Mouse position
 	sf::Vector2i mouse_pos_w;
+
 
 	//Private functions
 	void initialize_Variable();
